7bf104387617d446a2020a7b8e80a17b793fe474
Gaboubou <gab-attal@hotmail.fr>
2020-12-21 18:53:57 +0100

    resolve port 3000 problem

--- userapi/src/index.js
+++ userapi/src/index.js
@@ -1,28 +1,53 @@
-const express = require('express')
-const userRouter = require('./routes/user')
-const bodyParser = require('body-parser')
+const express = require('express');
+const userRouter = require('./routes/user');
+const bodyParser = require('body-parser');
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-const client = require('./dbClient')
+const client = require('./dbClient');
 client.on("error", (err) => {
-  console.error(err)
-})
+    console.error(err);
+});
 
 app.use(bodyParser.urlencoded({
-  extended: false
-}))
-app.use(bodyParser.json())
+    extended: false
+}));
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.use('/user', userRouter)
+app.use('/user', userRouter);
 
 const server = app.listen(port, (err) => {
-  if (err) throw err
-  console.log("Server listening the port " + port)
-})
+    if (err) throw err;
+    console.log("Server listening the port " + port);
+});
 
+process.on('SIGTERM', shutDown);
+process.on('SIGINT', shutDown);
 
-module.exports = server
+let connections = [];
+
+server.on('connection', connection => {
+    connections.push(connection);
+    connection.on('close', () => connections = connections.filter(curr => curr !== connection));
+});
+
+function shutDown() {
+    console.log('Received kill signal, shutting down gracefully');
+    server.close(() => {
+        console.log('Closed out remaining connections');
+        process.exit(0);
+    });
+
+    setTimeout(() => {
+        console.error('Could not close connections in time, forcefully shutting down');
+        process.exit(1);
+    }, 10000);
+
+    connections.forEach(curr => curr.end());
+    setTimeout(() => connections.forEach(curr => curr.destroy()), 5000);
+}
+
+module.exports = server;

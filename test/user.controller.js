140030617f7cab68af0acdfb9c83f21c4df2ac4a
Gaboubou <gab-attal@hotmail.fr>
2020-12-22 16:16:01 +0100

    test

--- test/user.controller.js
+++ test/user.controller.js
@@ -14,7 +14,7 @@ describe('User', () => {
       }
       userController.create(user, (err, result) => {
         expect(err).to.be.equal(null)
-        expect(result).to.be.equal(null)
+        expect(result).to.be.equal('OK')
         done()
       })
     })
